0c5d36f6a8e30409419006aa0959fb9b98ed4560
Kelvin Kong <kelvin86@gmail.com>
2022-07-18 16:12:43 +0800

    Added __init__ and fixed __all__

--- src/cvbox/fileio/__init__.py
+++ src/cvbox/fileio/__init__.py
@@ -2,6 +2,6 @@ from .json_dict import *
 from .permissions import * 
 from .pkl import *
 
-__all__ = [ json_dict.__all__    +
-            pkl.__all__          +
-            permissions.__all__ ]
+__all__ = json_dict.__all__      + \
+            pkl.__all__          + \
+            permissions.__all__ 
